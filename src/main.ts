5ae4eec8ae1d5d0eb1fde0253a7a77a89a2b1862
Mohammad Mostajeran <mostajeran.mohammad.1999@gmail.com>
2024-01-06 09:09:01 +0330

    fix grpc server

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-
-  app.connectMicroservice<MicroserviceOptions>({
+  const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'tasks',
       protoPath: join(__dirname, 'proto/tasks.proto'),
+      url: `0.0.0.0:${process.env.GRPC_PORT}`,
     },
   });
 
-  await app.listen(configService.get('GRPC_PORT'));
+  await app.listen();
 }
 bootstrap();
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskEntity } from './task.entity';
 import { AshlandBridgeModule } from 'src/ashland-bridge/ashland-bridge.module';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TaskEntity]), AshlandBridgeModule],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
